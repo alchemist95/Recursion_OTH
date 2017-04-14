0d85f21f524ec2a469ffaddf39586c330f9239c7
alchemist95 <shashank.9d@gmail.com>
2017-04-14 13:49:47 +0530

    FF

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -37,6 +37,7 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
+  config.assets.precompile += %w( .svg .eot .woff .ttf )
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
